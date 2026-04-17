8f7ed60d02f71c7bab5cc9ddbb377ccb1007e3ef
johancg04 <j.contreras.granados.04@gmail.com>
2026-04-17 18:54:09 -0500

    feat: add GreetDeveloperRequest record

---
+++ src/main/java/com/acme/hello/platform/generic/GreetDeveloperRequest.java
@@ -0,0 +1,25 @@
+package com.acme.hello.platform.generic;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Rest Resource representing incoming Greet Developer request data.
+ * @param firstName the developer's first name
+ * @param lastName the developer's last name
+ */
+public record GreetDeveloperRequest(String firstName,
+                                    String lastName) {
+    /**
+     * Constructs a new GreetDeveloperRequest instance with the given
+     * first and last name
+     * @param firstName the developer's first name
+     * @param lastName the developer's last name
+     */
+    @JsonCreator
+    public GreetDeveloperRequest(@JsonProperty("firstName") String firstName,
+                                 @JsonProperty("lastName") String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}

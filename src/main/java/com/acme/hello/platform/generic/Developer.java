ff6cde4b83a4bc3016b4d61ca405cd58fb3ffb5b
johancg04 <j.contreras.granados.04@gmail.com>
2026-04-17 18:42:52 -0500

    feat(developer): add developer entity in the 'Generic' bounded context

---
+++ src/main/java/com/acme/hello/platform/generic/Developer.java
@@ -0,0 +1,50 @@
+package com.acme.hello.platform.generic;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.UUID;
+
+/**
+ * Represents a Developer entity in the 'Generic' bounded context
+ * @author Open Source Application Development Team
+ */
+@Getter
+@Builder
+public class Developer {
+    private final UUID id = UUID.randomUUID();
+    private final String firstName;
+    private final String lastName;
+
+    /**
+     * Private constructor to enforce trimming of name fields
+     * @param firstName the developer's first name
+     * @param lastName the developer's last name
+     */
+    private Developer(String firstName, String lastName) {
+        this.firstName = StringUtils.trimToEmpty(firstName);
+        this.lastName = StringUtils.trimToEmpty(lastName);
+    }
+
+    /**
+     * Return the full name of the developer by
+     * concatenating firs and last name
+     */
+
+    public String getFullName() {return firstName + " " + lastName;}
+
+    /**
+     * Checks if either the first name or last name is blank (null, empty or white)
+     * @return true if any trimmed name is blank, false otherwise
+     */
+    public boolean isAnyNameBlank(){
+        return StringUtils.isAnyBlank(firstName, lastName);
+    }
+
+    /**
+     * Checks if either the first name or last name is empty
+     * @return true if any trimmed name is empty, false otherwise
+     */
+    public boolean isAnyNameEmpty(){return StringUtils.isAnyBlank(firstName, lastName);}
+}

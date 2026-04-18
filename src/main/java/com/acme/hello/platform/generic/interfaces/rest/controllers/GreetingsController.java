d51f8b4feae7edf96ae72207153aad520d348431
johancg04 <j.contreras.granados.04@gmail.com>
2026-04-17 20:25:39 -0500

    feat: add GreetingsController with GET and POST endpoints

---
+++ src/main/java/com/acme/hello/platform/generic/interfaces/rest/controllers/GreetingsController.java
@@ -0,0 +1,46 @@
+package com.acme.hello.platform.generic.interfaces.rest.controllers;
+
+import com.acme.hello.platform.generic.domain.model.entity.Developer;
+import com.acme.hello.platform.generic.interfaces.rest.assemblers.DeveloperAssembler;
+import com.acme.hello.platform.generic.interfaces.rest.assemblers.GreetDeveloperAssembler;
+import com.acme.hello.platform.generic.resources.GreetDeveloperRequest;
+import com.acme.hello.platform.generic.resources.GreetDeveloperResponse;
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+
+@RestController
+@RequestMapping("/api/v1/greetings")
+public class GreetingsController {
+    private static final Logger
+            LOGGER = org.slf4j.LoggerFactory.
+            getLogger(GreetingsController.class);
+
+    @GetMapping
+    public ResponseEntity<GreetDeveloperResponse> greetDeveloper(
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName) {
+        Developer developer = (Objects.isNull(firstName)) ||
+                                (Objects.isNull(lastName)) ?
+                null : Developer.builder().firstName(firstName)
+                        .lastName(lastName).build();
+
+        GreetDeveloperResponse response =
+                GreetDeveloperAssembler.toResourceFromEntity(developer);
+        LOGGER.info("Greeted developer: {}", response.fullName());
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<GreetDeveloperResponse> createGreeting(
+            @RequestBody GreetDeveloperRequest request) {
+        Developer developer = DeveloperAssembler.toEntityFromRequest(request);
+        GreetDeveloperResponse response =
+                GreetDeveloperAssembler.toResourceFromEntity(developer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
